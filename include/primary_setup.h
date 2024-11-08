757138cfdba9b3e972500dba80acc6455f4d0dec
Realvideogame <sing1168@purdue.edu>
2024-11-07 20:47:48 -0500

    assignments on header

--- include/primary_setup.h
+++ include/primary_setup.h
@@ -3,9 +3,10 @@
 // Function Protoypes
 
 // Setup-functions
-void setup_led_array(void);
-void setup_buttons(void);
-void setup_turn_timer(void);
-void setup_lcd(void);
+//pcb sexton
+void setup_led_array(void); // cecilie
+void setup_buttons(void); //tushy
+void setup_turn_timer(void); // bug
+void setup_lcd(void); //done
 void setup_dma(void);
 void setup_interboard_com_primary(void);
